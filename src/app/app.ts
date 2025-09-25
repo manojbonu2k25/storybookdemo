21fed4e01646e85d8b2800a3722a57db19d97fe5
Anastasia Sukhorebraya <anastasia.sukhorebraya@axelautomotive.com>
2025-09-25 11:00:10 -0700

    style: adjust button padding and add new button variant

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonComponent } from "../stories/button.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
